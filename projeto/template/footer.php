00b9d895baea83d08155dfafc42eae7cd164aeb8
EnzoZamineli <enzozamineli@gmail.com>
2024-11-15 23:32:36 -0300

    atualizacao_footer

--- projeto/template/footer.php
+++ projeto/template/footer.php
@@ -15,7 +15,7 @@
 </svg>
 
 <div class="container-fluid mt-auto">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-5 border-top">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-2 my-3 border-top" style="position: fixed; bottom: 0; width: 98%;">
     <div class="col-md-4 d-flex align-items-center">
       <a href="http://localhost:8080/SistemaDeApontamento_TCC/index.php" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
         <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
